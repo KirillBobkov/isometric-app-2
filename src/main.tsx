b28517fece80730493ea3efd54dfa2c528abe86d
Kirill Bobkov <KBobkov@alfabank.ru>
2025-02-09 22:28:34 +0400

    vite fix

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,13 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
-import { BrowserRouter } from 'react-router-dom';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { HashRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-          <BrowserRouter basename={'/isometric-app-2/'}>
-          <App />
-          </BrowserRouter>
-
+    <HashRouter>
+      <App />
+    </HashRouter>
   </StrictMode>
 );
